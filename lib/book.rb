2b517689ed1772a9438d2afd363c44805089d79f
William Pannell <lpann5@gmail.com>
2019-11-12 11:42:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,15 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
  
   def initialize(title)
     @title = title
   end
  
-  def title
-    @title
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-  
-  def author=(author)
-    @author = author
-  end
+ 
 end
 Book.new("And Then There Were None")
 
